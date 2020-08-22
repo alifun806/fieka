3ba1a3edc8af117e431e42e088bc974ce94172dd
alifun806 <69952765+alifun806@users.noreply.github.com>
2020-08-22 13:04:28 +0300

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,83 @@
+class Translation(object):
+    START_TEXT = """<u>بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ</u>
+       ➠ /Jihaad 🖲
+<b>JIHAAD</b> imekusanya kila aina ya ibada za kiroho na kiwiliwili, kuikinai dunia, kuihama nchi na kuyahama matamanio na hii ndiyo maana ikapewa jina la ‘Uchaji Allaah’, kwani imekuja katika Hadiyth kuwa:
+
+<b>‘Ucha Mungu wa umma wangu ni Jihaad katika njia ya Allaah.”</b>
+
+Na ndani yake mna kuitakasa nafsi, kuitakasa mali, na kumuuzia Allaah nafsi, na haya yote ni matunda ya mapenzi na imani na yakini na kuelekea kwa Allaah (Subhaanahu wa Ta’ala).
+
+NDUGU ZANGU KATIKA IMAAN MIMI NDUGU YENU NIME WATENGENEZEA ROBOT HII ILI MPATE KUJIFUNZA KWA WEPESI NA KWA WAKATI WOWOTE.
+
+Gusa hapa ➠ /hamis Kupata Vitabu 📖📚 Au darsa
+
+Mtume wa Allaah (Swalla Allaahu ‘alayhi wa sallam) amesema:
+<b>“Shahidi hahisi maumivu ya kuuliwa ila kama mmoja wenu anavyohisi maumivu ya kufinywa.”</b>
+
+Hayo  Na Mengine Allah Atakuonyesha Hapa ➠ /hamis au ➠ /jihaad:
+
+➠ /help Kwa Msaada zaidi..
+
+Kwa elimu zaidi  
+➠ @KhamisBot"""  
+    HELP_USER = """<b>Hapa ni sehemu ya Msaada</b>
+
+🖲 <a
+href='https://telegra.ph/i-love-islam-01-24'>I LOVE ISLAM</a>
+
+Kama una Hitaji Kusoma Au Kusiliza Qur'an Tukufu.
+❖ @Furqanbot
+
+<u>Kwa Darsa Mbali Mbali. 
+Txt 📄, Audio 🎧, Video & File N.k.</u>
+❖ @Hamisbot
+
+Kuongea Nasi Au Viongozi Tuandikie ✍.
+👉 @ViongoziBot Na Shidayako itafika Kwetu
+Kwa Idhini ya Allah Tutakusaidia In Shaa Allah.
+
+Allah Akujaalie Wepesi Katika Mambo yako na Akupe <b>Mwisho Mwema</b> Aamiyn"""
+
+    HAMIS_MAJIBU = """Jifunze Au Soma Ibaada Ya <a href='https://telegra.ph/Jihaad-01-24'>Jihaad</a> Kupitia hapa 
+
+
+
+1. <a
+href='https://telegra.ph/Jihaad-Maana-Ya-Jihaad-Na-Kuwekewa-Shariah-01-24'>Jihaad: Maana Ya Jihaad Na Kuwekewa Shari'ah</a>
+
+2. <a
+href='https://telegra.ph/Khawaarij-04-16'>Khawaarij</a>
+
+3. <a
+href='https://telegra.ph/Jihaad-Ni-Waajib-01-24'>Jihaad Ni Waajib</a>
+
+4. <a
+href='https://telegra.ph/Wajibu-Wa-Kuwa-Thabiti-01-24'>Wajibu Wa Kuwa Thabiti</a>
+
+5. <a
+href='https://telegra.ph/Fadhila-Za-Jihaad-01-24'>Fadhila Za Jihaad</a>
+
+6. <a 
+href='https://telegra.ph/Wajibu-Wa-Viongozi-Wa-Jeshi-01-24'>Wajibu Wa Viongozi Wa Jeshi</a>
+
+7. <a
+ href='https://telegra.ph/Al-Wahn-01-24'>Al-Wahn</a>
+
+
+8.<a
+ href='https://telegra.ph/Ribbiyyuwna-04-26'>ISLAMIC STATE</a>
+
+
+
+<a
+href='https://telegra.ph/KUJITOA-MUHANGA-02-21'>📚 KUJITOA MUHANGA 📖</a>
+
+1. <a
+href='https://telegra.ph/AMALIYAT-ISTISHAHADIYAH--KUJITOA-MUHANGA--KWA-MUJIBU-WA-QURAN--SUNNAH--NA-KAULI-MBALI-MBALI-ZA-WANACHUONI-02-21'>A'MALIYAT ISTISHAHADIYAH ( KUJITOA MUHANGA ) KWA MUJIBU WA QUR'AN , SUNNAH , NA KAULI MBALI MBALI ZA WANACHUONI</a>
+
+2. <a
+href='https://telegra.ph/UHALI-WA-KUJITOA-MUHANGA-KWA-MUJIBU-WA-QURAN-02-21'>SHARTI ZA KUJITOA MUHANGA :</a>
+
+<b><i><u>MWISHO</u></i></b>"""
+    WAKWELI = "https://t.me/KGposbot"
+    ALISWOD = "https://t.me/achia_bot"
